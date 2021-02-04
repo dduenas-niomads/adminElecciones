5ccb1dac4c10ffb07fa2a60b09af3f9a163eb8e2
daniel <dduenas@tumipos.com>
2021-02-04 15:08:17 -0500

    F

--- resources/views/areas/index.blade.php
+++ resources/views/areas/index.blade.php
@@ -1,49 +1,74 @@
-@extends('base')
-@extends('layouts.app')
-@section('main')
+@extends('adminlte::page')
 
-<div class="col-sm-12">
-  @if(session()->get('success'))
-    <div class="alert alert-success">
-      {{ session()->get('success') }}  
-    </div>
-  @endif
-</div>
+@section('title', 'Áreas')
+
+@section('content_header')
+    <h1>Listado de áreas     <a style="margin: 19px;" href="{{ route('areas.create')}}" class="btn btn-primary">Nueva área</a> </h1>
+@stop
+
+@section('nav-areas')
+    active
+@stop
+
+@section('content')
+
 <div class="row">
-<div class="col-sm-12">
-    <h1 class="display-3">Áreas</h1>  
-    <div>
-    <a style="margin: 19px;" href="{{ route('areas.create')}}" class="btn btn-primary">Nuevo Área</a>
-    </div>  
-  <table class="table table-striped">
-    <thead>
-        <tr>
-          <td>ID</td>
-          <td>Nombre</td>
-          <td>Código</td>
-          <td colspan = 2>Acciones</td>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach($areas as $area)
-        <tr>
-            <td>{{$area->id}}</td>
-            <td>{{$area->name}}</td>
-            <td>{{$area->code}}</td>
-            <td>
-                <a href="{{ route('areas.edit',$area->id)}}" class="btn btn-primary">Editar</a>
-            </td>
-            <td>
-                <form action="{{ route('areas.destroy', $area->id)}}" method="post">
-                  @csrf
-                  @method('DELETE')
-                  <button class="btn btn-danger" type="submit">Eliminar</button>
-                </form>
-            </td>
-        </tr>
-        @endforeach
-    </tbody>
-  </table>
-<div>
+		<div class="col-12">
+			<div class="card">
+				<div class="card-body">
+
+          <div class="col-sm-12">
+            @if(session()->get('success'))
+              <div class="alert alert-success">
+                {{ session()->get('success') }}  
+              </div>
+            @endif
+          </div>
+          <div class="row">
+          <div class="col-sm-12">
+            <table class="table">
+              <thead>
+                  <tr>
+                    <td>ID</td>
+                    <td>Nombre</td>
+                    <td>Código</td>
+                    <td colspan = 2>Acciones</td>
+                  </tr>
+              </thead>
+              <tbody>
+                  @foreach($areas as $area)
+                  <tr>
+                      <td>{{$area->id}}</td>
+                      <td>{{$area->name}}</td>
+                      <td>{{$area->code}}</td>
+                      <td>
+                          <a href="{{ route('areas.edit',$area->id)}}" class="btn btn-primary">Editar</a>
+                      </td>
+                      <td>
+                          <form action="{{ route('areas.destroy', $area->id)}}" method="post">
+                            @csrf
+                            @method('DELETE')
+                            <button class="btn btn-danger" type="submit">Eliminar</button>
+                          </form>
+                      </td>
+                  </tr>
+                  @endforeach
+              </tbody>
+            </table>
+          <div>
+          </div>
+        </div>
+      </div>
+    </div>
 </div>
-@endsection
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
+
+
--- resources/views/vendor/adminlte/partials/sidebar/left-sidebar.blade.php
+++ resources/views/vendor/adminlte/partials/sidebar/left-sidebar.blade.php
@@ -8,20 +8,90 @@
     @endif
 
     {{-- Sidebar menu --}}
+    
+    <!-- Sidebar -->
     <div class="sidebar">
+        <!-- Sidebar Menu -->
+        @php
+            $user = \Auth::user();
+        @endphp
+        <!-- Sidebar Menu -->
         <nav class="mt-2">
-            <ul class="nav nav-pills nav-sidebar flex-column {{ config('adminlte.classes_sidebar_nav', '') }}"
-                data-widget="treeview" role="menu"
-                @if(config('adminlte.sidebar_nav_animation_speed') != 300)
-                    data-animation-speed="{{ config('adminlte.sidebar_nav_animation_speed') }}"
-                @endif
-                @if(!config('adminlte.sidebar_nav_accordion'))
-                    data-accordion="false"
-                @endif>
-                {{-- Configured sidebar links --}}
-                @each('adminlte::partials.sidebar.menu-item', $adminlte->menu('sidebar'), 'item')
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu">
+                <!-- Add icons to the links using the .nav-icon class
+                    with font-awesome or any other icon font library -->
+                <li class="nav-item has-treeview @yield('nav-dashboard-menu')">
+                    <a href="#" class="nav-link @yield('nav-dashboard')">
+                        <i class="nav-icon fas fa-tachometer-alt"></i>
+                        <p>
+                            Panel de control
+                            <i class="right fas fa-angle-left"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="/home" class="nav-link @yield('nav-overview')">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Visión general</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="/my-account" class="nav-link @yield('nav-account')">
+                                <i class="far fa-id-card nav-icon"></i>
+                                <p>Mi cuenta</p>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu">
+                <li class="nav-item">
+                    <a href="/areas" class="nav-link  @yield('nav-areas')">
+                        <i class="fa fa-store-alt nav-icon "></i>
+                        <p>Áreas</p>
+                    </a>
+                </li>
+            </ul>
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu">
+                <li class="nav-item">
+                    <a href="/products" class="nav-link  @yield('nav-products')">
+                        <i class="fa fa-shopping-cart nav-icon "></i>
+                        <p>Productos</p>
+                    </a>
+                </li>
+            </ul>
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu">
+                <li class="nav-item">
+                    <a href="/orders" class="nav-link  @yield('nav-orders')">
+                        <i class="fa fa-clipboard-check nav-icon "></i>
+                        <p>Mis Órdenes</p>
+                    </a>
+                </li>
+            </ul>
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu">
+                <li class="nav-item">
+                    <a href="/payments" class="nav-link  @yield('nav-payments')">
+                        <i class="fa fa-file-invoice nav-icon "></i>
+                        <p>Mis pagos</p>
+                    </a>
+                </li>
+            </ul>
+            <!-- <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu">
+                <li class="nav-item">
+                    <a href="/contact-support" class="nav-link  @yield('nav-contact-support')">
+                        <i class="fa fa-headset nav-icon "></i>
+                        <p>Soporte técnico</p>
+                    </a>
+                </li>
+            </ul> -->
+        </nav>
+        <!-- /.sidebar-menu -->
+        <nav class="mt-2" style=" bottom: 0;">
+            <ul class="nav nav-pills nav-sidebar flex-column">
+                <li class="nav-item">
+                    <p  style="color: #ffffff;">© 2020 {{ env('APP_NAME') }}</p>
+                </li>
             </ul>
         </nav>
     </div>
-
 </aside>
