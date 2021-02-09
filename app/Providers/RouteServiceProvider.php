e9a8a772e0ec331213129d83f11b30fc4ab13718
Alvaro Acosta <amacostas92@gmail.com>
2021-02-09 12:56:11 -0500

    voter login

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -18,6 +18,7 @@ class RouteServiceProvider extends ServiceProvider
      * @var string
      */
     public const HOME = '/home';
+    public const VOTE = '/vote';
 
     /**
      * The controller namespace for the application.
