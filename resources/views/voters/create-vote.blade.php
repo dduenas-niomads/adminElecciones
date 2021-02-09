01676058cf1293c4c96ba257cd89343332815b98
daniel <dduenas@tumipos.com>
2021-02-09 16:02:13 -0500

    F

--- resources/views/voters/create-vote.blade.php
+++ resources/views/voters/create-vote.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.vote')
+@extends('layouts.vote-datatable')
 
 @section('content')
 <div class="container">
@@ -7,7 +7,7 @@
             <div class="card">
                 <div class="card-header">Crear voto - {{ $voter->code }}</div>
                 <div class="card-body">
-                    <table class="table" id="table">
+                    <table class="table" id="example1">
                         <thead>
                             <th>NOMBRE</th>
                             <th>CÓDIGO</th>
@@ -27,32 +27,42 @@
             </div>
         </div>
     </div>
-</div><!-- css -->
-<link rel="stylesheet" href="{{ asset('css/datatables/dataTables.bootstrap4.min.css') }}">
-<link rel="stylesheet" href="{{ asset('css/datatables/responsive.bootstrap4.min.css') }}">
-<!-- scripts -->
-<script src="{{ asset('scripts/datatables/jquery.dataTables.min.js') }}"></script>
-<script src="{{ asset('scripts/datatables/dataTables.bootstrap4.min.js') }}"></script>
-<script src="{{ asset('scripts/datatables/dataTables.responsive.min.js') }}"></script>
-<script src="{{ asset('scripts/datatables/responsive.bootstrap4.min.js') }}"></script>
-<script>
-    $(document).ready(function (e) {
-        
-        $("#table").DataTable({
-            "info": true,
-            "scrollX": false,
-            "ordering": false,
-            "searching": true,
-            "processing": true,
-            "serverSide": false,
-            "lengthChange": false,
-            "bPaginate": true,
-            "responsive": false,
-            "language": {
-                "url": "/js/languages/datatables/es.json"
-            },
-            "order": [[ 1, "asc" ]],
-        });
-    });
-</script>
+</div>
 @endsection
+
+@section('css') 
+    <link rel="stylesheet" href="{{ asset('css/datatables/dataTables.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/datatables/responsive.bootstrap4.min.css') }}">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+
+@section('js')
+   <!-- scripts -->
+    <script src="{{ asset('scripts/datatables/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('scripts/datatables/dataTables.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('scripts/datatables/dataTables.responsive.min.js') }}"></script>
+    <script src="{{ asset('scripts/datatables/responsive.bootstrap4.min.js') }}"></script>
+    <script>
+        $(document).ready(function (e) {
+            
+            $("#example1").DataTable({
+                "info": true,
+                "scrollX": false,
+                "ordering": false,
+                "searching": true,
+                "processing": true,
+                "serverSide": false,
+                "lengthChange": false,
+                "bPaginate": true,
+                "responsive": false,
+                "language": {
+                    "url": "/js/languages/datatables/es.json"
+                },
+                "order": [[ 1, "asc" ]],
+            });
+        });
+    </script>
+@stop
+@stop
--- resources/views/voters/login.blade.php
+++ resources/views/voters/login.blade.php
@@ -20,12 +20,6 @@
                             </div>
                         </div>
                     </div>
-                    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-                    <script>
-                        $(document).ready(function (e) {
-                            $('#modal-info').modal({ backdrop: 'static', keyboard: false });                  
-                        });
-                    </script>
                 @endif
                 <div class="card-body">
                     <form method="POST" action="{{ route('voter-login-post') }}">
@@ -59,3 +53,13 @@
     </div>
 </div>
 @endsection
+
+
+@if (isset($error) && $error)
+<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+<script>
+    $(document).ready(function (e) {
+        $('#modal-info').modal({ backdrop: 'static', keyboard: false });                  
+    });
+</script>
+@endif
