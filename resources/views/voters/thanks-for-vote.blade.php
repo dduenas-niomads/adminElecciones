d9adfc63854ba65fb73b20e2af6f969a8762015f
daniel <dduenas@tumipos.com>
2021-02-10 11:27:46 -0500

    F

--- resources/views/voters/thanks-for-vote.blade.php
+++ resources/views/voters/thanks-for-vote.blade.php
@@ -5,22 +5,6 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Inicio de sesión de votante</div>
-
-                @if (isset($error) && $error)
-                    <div class="modal" tabindex="-1" role="dialog" id="modal-info">
-                        <div class="modal-dialog" role="document">
-                            <div class="modal-content" >
-                            <div class="modal-body ">
-                                <p>{{ isset($message) ? $message : "Mensaje importante" }}</p>
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-danger" data-dismiss="modal">Volver a intentar</button>
-                            </div>
-                            </div>
-                        </div>
-                    </div>
-                @endif
                 <div class="card-body">
                     <form method="GET" action="{{ route('voter-login') }}">
                         @csrf
