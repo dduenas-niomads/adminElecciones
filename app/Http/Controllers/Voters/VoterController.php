cacb095a78824d44d5e01eaa3c1d7046fd0fee1f
daniel <dduenas@tumipos.com>
2021-02-28 11:37:57 -0500

    F

--- app/Http/Controllers/Voters/VoterController.php
+++ app/Http/Controllers/Voters/VoterController.php
@@ -67,7 +67,7 @@ class VoterController extends Controller
         try {
             $result->notify(new SendVoteResume($result));
         } catch (\Throwable $th) {
-            return;
+            return null;
         }
     }
 
@@ -76,7 +76,7 @@ class VoterController extends Controller
         try {
             $voter->notify(new SendVoteResume($voter));
         } catch (\Throwable $th) {
-            return;
+            throw $th;
         }
     }
 
